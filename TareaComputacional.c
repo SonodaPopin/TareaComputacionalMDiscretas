44ef7478c1dede3a0f91aa44d2b9937672341db7
igbarria1314 <114494885+igbarria1314@users.noreply.github.com>
2023-12-06 23:46:37 -0300

    Update TareaComputacional.c

--- TareaComputacional.c
+++ TareaComputacional.c
@@ -7,18 +7,18 @@
 #include <stdbool.h>
 #include <stdio.h>
  
-// Number of vertices in the graph
+// Numero de vertices en el grafo
 #define V 23
  int to_int(char c[20]){
-    if(strcmp(c,"Arturo Prat")==0) return 0;//desde art prat
+    if(strcmp(c,"Arturo_Prat")==0) return 0;//desde art prat
     if(strcmp(c,"Serrano")==0) return 1;
     if(strcmp(c,"Salas")==0) return 2;
     if(strcmp(c,"Angol")==0) return 3;
     if(strcmp(c,"Lincoyan")==0) return 4;
     if(strcmp(c,"Rengo")==0) return 5;
     if(strcmp(c,"Caupolican")==0) return 6;
-    if(strcmp(c,"Anival Pinto")==0) return 7;
-    if(strcmp(c,"Colo Colo")==0) return 8;
+    if(strcmp(c,"Anibal_Pinto")==0) return 7;
+    if(strcmp(c,"Colo_Colo")==0) return 8;
     if(strcmp(c,"Castellon")==0) return 9;
     if(strcmp(c,"Tucapel")==0) return 10;
     if(strcmp(c,"Orompello")==0) return 11;
@@ -26,17 +26,17 @@
     if(strcmp(c,"Paicavi")==0) return 13;//hasta paicavi estan en paralelo
     if(strcmp(c,"Chacabuco")==0) return 14;
     if(strcmp(c,"Cochrane")==0) return 15;
-    if(strcmp(c,"San Martin")==0) return 16;
+    if(strcmp(c,"San_Martin")==0) return 16;
     if(strcmp(c,"O'Higgins")==0) return 17;
-    if(strcmp(c,"Barros Arana")==0) return 18;
+    if(strcmp(c,"Barros_Arana")==0) return 18;
     if(strcmp(c,"Freire")==0) return 19;
     if(strcmp(c,"Maipu")==0) return 20;
-    if(strcmp(c,"Los Carrera")==0) return 21;
-    if(strcmp(c,"Pedro Aguirre Cerda")==0) return 22;
+    if(strcmp(c,"Los_Carrera")==0) return 21;
+    if(strcmp(c,"Pedro_Aguirre_Cerda")==0) return 22;
 }
-// A utility function to find the vertex with minimum
-// distance value, from the set of vertices not yet included
-// in shortest path tree
+
+// Una funcion de utilidad para encontrar el vertice con minima
+// valor de distancia.
 int minDistance(int dist[], bool sptSet[]){
     // Initialize min value
     int min = INT_MAX, min_index;
@@ -48,52 +48,22 @@ int minDistance(int dist[], bool sptSet[]){
     return min_index;
 }
  
-// A utility function to print the constructed distance
-// array
+// Funcion utilizada para imprimir distancias
 void printSolution(int dist[], int final){
     printf("Distancia del origen es:\t\t al vertice\n");
     printf("%d \t\t\t\t %d\n", dist[final],final);
 }
- 
-// Function that implements Dijkstra's single source
-// shortest path algorithm for a graph represented using
-// adjacency matrix representation
 void dijkstra(int graph[V][V], int src, int final){
-    int dist[V]; // The output array.  dist[i] will hold the
-                 // shortest
-    // distance from src to i
+    int dist[V];
  
-    bool sptSet[V]; // sptSet[i] will be true if vertex i is
-                    // included in shortest
-    // path tree or shortest distance from src to i is
-    // finalized
- 
-    // Initialize all distances as INFINITE and stpSet[] as
-    // false
+    bool sptSet[V];
     for (int i = 0; i < V; i++)
         dist[i] = INT_MAX, sptSet[i] = false;
- 
-    // Distance of source vertex from itself is always 0
     dist[src] = 0;
- 
-    // Find shortest path for all vertices
     for (int count = 0; count < V - 1; count++) {
-        // Pick the minimum distance vertex from the set of
-        // vertices not yet processed. u is always equal to
-        // src in the first iteration.
         int u = minDistance(dist, sptSet);
- 
-        // Mark the picked vertex as processed
         sptSet[u] = true;
- 
-        // Update dist value of the adjacent vertices of the
-        // picked vertex.
         for (int v = 0; v < V; v++)
- 
-            // Update dist[v] only if is not in sptSet,
-            // there is an edge from u to v, and total
-            // weight of path from src to  v through u is
-            // smaller than current value of dist[v]
             if (!sptSet[v] && graph[u][v]
                 && dist[u] != INT_MAX
                 && dist[u] + graph[u][v] < dist[v])
@@ -128,8 +98,13 @@ int main()
 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0},//carrera limite arriba
 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};//diagonal ;
     char *token;
-    char str[100]; 
-    fgets(str, sizeof(str), stdin);
+    char str[100];
+    /*Para hacer la consulta se utiliza nombre claros por ejemplo:
+    Serrano 100 Rengo 200
+    o
+    Colo_colo 500 San_Martin 100
+    en el caso de que hayan espacios entre el nombre de la calle necesitamos poner un barra baja "_"*/
+    fgets(str, sizeof(str), stdin); 
     int i=0;
     token = strtok(str," ");
     char *c1,*c2,*c3,*c4;
